852196775004a27a6dc91d405dc3bc9e63c4545d
Justin Schlabach <me@justinschlabach.com>
2023-08-21 10:02:12 -0400

    Removed comment continuation as well as a few other changes.
    Signed-off-by: Justin Schlabach <me@justinschlabach.com>

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -16,9 +16,16 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.dap.core" },
+    -- { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-    -- { import = "lazyvim.plugins.extras.ui.edgy" },
+    --{ import = "lazyvim.plugins.extras.ui.edgy" },
+    {
+      import = "lazyvim.plugins.extras.coding.copilot",
+      opts = {
+        suggestion = { enabled = true },
+        panel = { enabled = true },
+      },
+    },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- .config/nvim/lua/plugins/jutt.lua
+++ .config/nvim/lua/plugins/jutt.lua
@@ -135,29 +135,4 @@ return {
       { "<leader>ut", vim.cmd.UndotreeToggle, mode = "n", desc = "Undo Tree" },
     },
   },
-
-  {
-    "numToStr/Comment.nvim",
-    keys = {
-      { "gcc", mode = "n", desc = "Comment toggle current line" },
-      { "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
-      { "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
-      { "gbc", mode = "n", desc = "Comment toggle current block" },
-      { "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
-      { "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
-      {
-        "<leader>/",
-        function()
-          require("Comment.api").toggle.linewise.current()
-        end,
-        mode = "n",
-        desc = "Toggle Comment",
-      },
-      {
-        "<leader>/",
-        "<ESC><CMD>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-        mode = "v",
-      },
-    },
-  },
 }
