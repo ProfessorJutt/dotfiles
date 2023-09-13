b38852c2f7c8dd83ab73caf25f9e22563792d0d8
Justin Schlabach <me@justinschlabach.com>
2023-09-13 10:19:54 -0400

    Stopping the autocomplete from eating my tabs.

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -13,3 +13,17 @@ vim.api.nvim_create_autocmd("BufEnter", {
   group = augroup("commentline"),
   desc = "Disable New Line Comment",
 })
+
+-- Stop snippet from perpetuating forever and eating my tab keys...
+vim.api.nvim_create_autocmd("ModeChanged", {
+  pattern = "*",
+  callback = function()
+    if
+      ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+      and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+      and not require("luasnip").session.jump_active
+    then
+      require("luasnip").unlink_current()
+    end
+  end,
+})
