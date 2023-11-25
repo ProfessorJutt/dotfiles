e4cce8a917179a7528d46a51b0787227446ca241
Jutt <professorjutt@gmail.com>
2023-11-25 00:29:09 -0500

    Adding some missed files.

---
+++ .config/baldie_nvim/lua/jutt/core/globals.lua
@@ -0,0 +1,4 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.skip_ts_context_commentstring_module = true
+vim.g.codeium_manual = true
---
+++ .config/baldie_nvim/lua/jutt/core/lazy.lua
@@ -0,0 +1,43 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local opts = {
+	defaults = {
+		lazy = false,
+	},
+	install = {
+		colorscheme = { "nightfly" },
+	},
+	rtp = {
+		disabled_plugins = {
+			"gzip",
+			"matchit",
+			"matchparen",
+			"netrw",
+			"netrwPlugin",
+			"tarPlugin",
+			"tohtml",
+			"tutor",
+			"zipPlugin",
+		},
+	},
+	change_detection = {
+		notify = false,
+	},
+}
+
+require("lazy").setup({
+	{ import = "jutt.plugins" },
+	{ import = "jutt.plugins.lsp" },
+}, opts)
