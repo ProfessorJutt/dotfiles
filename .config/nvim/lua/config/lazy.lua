21563f15bf67b90c6f2bae653b1ee33244d05ec3
Justin Schlabach <me@justinschlabach.com>
2023-12-07 16:56:06 -0500

    Adding my font choice and alpha changes.

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.yanky" },
     -- { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    { import = "lazyvim.plugins.extras.ui.alpha" },
     --{ import = "lazyvim.plugins.extras.ui.edgy" },
     -- {
     --   import = "lazyvim.plugins.extras.coding.copilot",
