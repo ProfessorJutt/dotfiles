814e44c3717e7dc3a34be1d5398d77442617d3a5
Justin Schlabach <me@justinschlabach.com>
2025-04-25 15:11:02 -0400

    Blink swap

--- config/baldie_nvim/lua/jutt/plugins/cmp.lua
+++ config/baldie_nvim/lua/jutt/plugins/cmp.lua
@@ -1,67 +1,107 @@
+-- return {
+-- 	"hrsh7th/nvim-cmp",
+-- 	event = "InsertEnter",
+-- 	dependencies = {
+-- 		{
+-- 			"L3MON4D3/LuaSnip",
+-- 			version = "v2.*",
+-- 			build = "make install_jsregexp",
+-- 		},
+-- 		"saadparwaiz1/cmp_luasnip", -- for autocompletion
+--
+-- 		"hrsh7th/cmp-nvim-lsp",
+-- 		"hrsh7th/cmp-buffer", -- source for text in buffer
+-- 		"hrsh7th/cmp-path", -- source for file system paths
+--
+-- 		"rafamadriz/friendly-snippets", -- useful snippets
+-- 		"onsails/lspkind.nvim", -- vs-code like pictograms
+-- 	},
+-- 	config = function()
+-- 		local cmp = require("cmp")
+-- 		local luasnip = require("luasnip")
+-- 		local lspkind = require("lspkind")
+--
+-- 		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+-- 		require("luasnip.loaders.from_vscode").lazy_load()
+--
+-- 		cmp.setup({
+-- 			completion = {
+-- 				completeopt = "menu,menuone,popup",
+-- 			},
+-- 			window = {
+-- 				completion = cmp.config.window.bordered(),
+-- 				documentation = cmp.config.window.bordered(),
+-- 			},
+-- 			snippet = {
+-- 				expand = function(args)
+-- 					luasnip.lsp_expand(args.body)
+-- 				end,
+-- 			},
+-- 			mapping = cmp.mapping.preset.insert({
+-- 				["<C-p>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+-- 				["<C-n>"] = cmp.mapping.select_next_item(), -- next suggestion
+-- 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+-- 				["<C-f>"] = cmp.mapping.scroll_docs(4),
+-- 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+-- 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
+-- 				["<CR>"] = cmp.mapping.confirm({ select = false }),
+-- 			}),
+-- 			-- sources for autocompletion
+--
+-- 			sources = cmp.config.sources({
+-- 				{ name = "nvim_lsp" },
+-- 				{ name = "luasnip" }, -- snippets
+-- 				{ name = "buffer" }, -- text within current buffer
+-- 				{ name = "path" }, -- file system paths
+-- 			}),
+--
+-- 			-- configure lspkind for vs-code like pictograms in completion menu
+-- 			formatting = {
+-- 				format = lspkind.cmp_format({
+-- 					maxwidth = 100,
+-- 					ellipsis_char = "...",
+-- 				}),
+-- 			},
+-- 		})
+-- 	end,
+-- }
+
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		{
-			"L3MON4D3/LuaSnip",
-			version = "v2.*",
-			build = "make install_jsregexp",
+	"saghen/blink.cmp",
+	dependencies = { "rafamadriz/friendly-snippets" },
+	version = "1.*",
+
+	opts = {
+		-- See :h blink-cmp-config-keymap for defining your own keymap
+		keymap = { preset = "enter" },
+
+		appearance = {
+			nerd_font_variant = "mono",
 		},
-		"saadparwaiz1/cmp_luasnip", -- for autocompletion
 
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer", -- source for text in buffer
-		"hrsh7th/cmp-path", -- source for file system paths
+		completion = {
+			documentation = { auto_show = true },
+			menu = {
+				draw = {
+					padding = { 0, 1 },
+					components = {
+						kind_icon = {
+							text = function(ctx)
+								return " " .. ctx.kind_icon .. ctx.icon_gap .. " "
+							end,
+						},
+					},
+				},
+			},
+		},
 
-		"rafamadriz/friendly-snippets", -- useful snippets
-		"onsails/lspkind.nvim", -- vs-code like pictograms
+		-- Default list of enabled providers defined so that you can extend it
+		-- elsewhere in your config, without redefining it, due to `opts_extend`
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+
+		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		local lspkind = require("lspkind")
-
-		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-		require("luasnip.loaders.from_vscode").lazy_load()
-
-		cmp.setup({
-			completion = {
-				completeopt = "menu,menuone,popup",
-			},
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-p>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				["<C-n>"] = cmp.mapping.select_next_item(), -- next suggestion
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-			}),
-			-- sources for autocompletion
-
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" }, -- snippets
-				{ name = "buffer" }, -- text within current buffer
-				{ name = "path" }, -- file system paths
-			}),
-
-			-- configure lspkind for vs-code like pictograms in completion menu
-			formatting = {
-				format = lspkind.cmp_format({
-					maxwidth = 100,
-					ellipsis_char = "...",
-				}),
-			},
-		})
-	end,
+	opts_extend = { "sources.default" },
 }
--- config/baldie_nvim/lua/jutt/plugins/lsp/lspconfig.lua
+++ config/baldie_nvim/lua/jutt/plugins/lsp/lspconfig.lua
@@ -1,98 +1,36 @@
 return {
 	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		{ "williamboman/mason.nvim", config = true },
-		"williamboman/mason-lspconfig.nvim",
+	dependencies = { "saghen/blink.cmp" },
 
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-
-    { "folke/neodev.nvim", opts = {} },
-		{ "j-hui/fidget.nvim", opts = {} },
-
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	config = function()
-		local lspconfig = require("lspconfig")
-		local mason_lspconfig = require("mason-lspconfig")
-
-		mason_lspconfig.setup({
-			ensure_installed = {
-				"html",
-				"cssls",
-				"tailwindcss",
-				"lua_ls",
-				"dockerls",
-				"docker_compose_language_service",
-				"terraformls",
-				"yamlls",
-				"ruff",
-				"basedpyright",
-				"bashls",
-				"gopls",
-				-- "vtsls",
-			},
-		})
-
-
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-			callback = function(ev)
-				local map = function(keys, func, desc, mode)
-					mode = mode or "n"
-					vim.keymap.set(mode, keys, func, { buffer = ev.buf, desc = "LSP: " .. desc })
-				end
-
-				local telescope = require("telescope.builtin")
-
-				map("gR", telescope.lsp_references, "Show references")
-				map("gD", vim.lsp.buf.declaration, "Go to declaration")
-				map("gd", telescope.lsp_definitions, "Show definitions")
-				map("gi", telescope.lsp_implementations, "Show implementations")
-				map("gt", telescope.lsp_type_definitions, "Show type definitions")
-				-- map("K", vim.lsp.buf.hover, "Show documentation for what is under cursor")
-				map("<leader>ca", vim.lsp.buf.code_action, "See available code actions")
-				map("<leader>rn", vim.lsp.buf.rename, "Smart rename")
-				-- map("<leader>d", vim.diagnostic.open_float, "Show line diagnostics")
-
-			end,
-		})
-
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-		-- used to enable autocompletion (assign to every lsp server config)
-		local capabilities = cmp_nvim_lsp.default_capabilities()
-
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
-
-		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-			["emmet_language_server"] = function()
-				lspconfig["emmet_language_server"].setup({
-					capabilities = capabilities,
-					filetypes = {
-						"html",
-						"typescriptreact",
-						"javascriptreact",
-						"css",
-						"sass",
-						"scss",
-						"less",
-						"svelte",
+	opts = {
+		servers = {
+			basedpyright = {
+				settings = {
+					basedpyright = {
+						typeCheckingMode = "standard",
 					},
-				})
-			end,
-		})
+				},
+			},
+			emmet_language_server = {
+				filetypes = {
+					"html",
+					"typescriptreact",
+					"javascriptreact",
+					"css",
+					"sass",
+					"scss",
+					"less",
+					"svelte",
+				},
+			},
+		},
+	},
+	config = function(_, opts)
+		local lspconfig = require("lspconfig")
+
+		for server, config in pairs(opts.servers) do
+			config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+			lspconfig[server].setup(config)
+		end
 	end,
 }
